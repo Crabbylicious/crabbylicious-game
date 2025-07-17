ca459acd7ecb92e5eae433bc580651bbfe6ebcba
dawnhazed <shafadoesthings@gmail.com>
2025-07-17 18:23:38 +0700

    background, ground, and title

---
+++ Crabbylicious/Nodes/UI/TitleNode.swift
@@ -0,0 +1,22 @@
+//
+//  TitleNode.swift
+//  Crabbylicious
+//
+//  Created by Nadaa Shafa Nadhifa on 17/07/25.
+//
+
+import GameplayKit
+import SpriteKit
+
+class TitleNode: SKSpriteNode {
+  init(size: CGSize) {
+    let texture = SKTexture(imageNamed: "title")
+    super.init(texture: texture, color: .clear, size: size)
+    print("title appeared ✅")
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
--- Crabbylicious/Scenes/HomeScene.swift
+++ Crabbylicious/Scenes/HomeScene.swift
@@ -5,21 +5,40 @@
 //  Created by Java Kanaya Prada on 16/07/25.
 //
 
+import GameplayKit
 import SpriteKit
+import SwiftUI
 
 class HomeScene: SKScene {
+  let gameArea: CGRect
+  
+  override init(size: CGSize) {
+    let maxAspectRatio: CGFloat = 16.0 / 9.0
+    let playableWidth = size.height / maxAspectRatio
+    let margin = (size.width - playableWidth) / 2
+    gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+
+    super.init(size: size)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func didMove(to view: SKView) {
-    super.didMove(to: view)
-    setupScene()
-    setupUI()
-  }
-
-  private func setupScene() {
-    // TODO: Setup scene properties
-  }
-
-  private func setupUI() {
-    // TODO: Setup home screen UI
+    let background = BackgroundNode(size: size)
+    background.zPosition = 0
+    addChild(background)
+    
+    let ground = GroundNode (size: size)
+    ground.zPosition = 1
+    addChild(ground)
+    
+    let title = TitleNode(size: CGSize(width: 300, height: 300))
+    title.position = CGPoint(x: gameArea.midX, y: gameArea.midY)
+    title.zPosition = 10
+    addChild(title)
   }
 
   override func touchesBegan(_: Set<UITouch>, with _: UIEvent?) {
