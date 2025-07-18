864016ed6a41313e8b58e0eee91080bcf92a255e
javaprada <javakanaya@gmail.com>
2025-07-18 17:08:02 +0700

    docs: remove comments

--- Crabbylicious/Views/ContentView.swift
+++ Crabbylicious/Views/ContentView.swift
@@ -11,8 +11,6 @@ import SwiftUI
 struct ContentView: View {
   var scene: SKScene {
     let screenSize = UIScreen.main.bounds.size
-
-    // Use debug scene temporarily
     let scene = ECSGameScene(size: screenSize)
     scene.scaleMode = .aspectFill
     return scene
