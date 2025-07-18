c21194a726eb3882ecafc8bdcbf4e925b61d5fea
Nessa <nessaprogramming@gmail.com>
2025-07-18 10:34:14 +0700

    Setup legs crab animation

--- Crabbylicious/Nodes/Game/CrabNode.swift
+++ Crabbylicious/Nodes/Game/CrabNode.swift
@@ -10,15 +10,43 @@ import GameplayKit
 import SpriteKit
 
 class CrabNode: SKSpriteNode {
-  init(size _: CGSize) {
-    let texture = SKTexture(imageNamed: "crabAndBowl2")
-    super.init(texture: texture, color: .clear, size: texture.size())
-    setScale(0.15)
-    zPosition = 2
-  }
+    private let legsNode: SKSpriteNode
+    private var legAnimationActionKey = "legAnimation"
+    private var legTextures: [SKTexture] = []
 
-  @available(*, unavailable)
-  required init?(coder _: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    init(size: CGSize) {
+        let texture = SKTexture(imageNamed: "crabAndBowl")
+        let atlas = SKTextureAtlas(named: "Legs")
+        legTextures = [
+            atlas.textureNamed("leg1"),
+            atlas.textureNamed("leg2")
+        ]
+        legsNode = SKSpriteNode(texture: legTextures[0])
+        
+        super.init(texture: texture, color: .clear, size: texture.size())
+        self.setScale(0.2)
+        self.zPosition = 2
+        
+        // posisi dan ukuran legs crab
+        legsNode.setScale(0.6)
+        legsNode.position = CGPoint(x: 0, y: -self.size.height * 1.9) 
+        legsNode.zPosition = 3
+        addChild(legsNode)
+    }
+
+    func startLegAnimation() {
+        guard legsNode.action(forKey: legAnimationActionKey) == nil else { return }
+        let animation = SKAction.repeatForever(SKAction.animate(with: legTextures, timePerFrame: 0.15))
+        legsNode.run(animation, withKey: legAnimationActionKey)
+    }
+
+    func stopLegAnimation() {
+        legsNode.removeAction(forKey: legAnimationActionKey)
+        legsNode.texture = legTextures[0] // reset posisi legs
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
