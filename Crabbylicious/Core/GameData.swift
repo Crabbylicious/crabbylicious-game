592b57c1f03b010efb6c679bdde9465be8d0d970
javaprada <javakanaya@gmail.com>
2025-07-25 14:30:01 +0700

    feat: more ingredients and recipes

--- Crabbylicious/Core/GameData.swift
+++ Crabbylicious/Core/GameData.swift
@@ -16,37 +16,48 @@ class GameData {
   static let lontong = Ingredient(imageName: "lontong", name: "lontong", isAbsurd: false)
   static let tahu = Ingredient(imageName: "tahu", name: "tahu", isAbsurd: false)
   static let telor = Ingredient(imageName: "telor", name: "telor", isAbsurd: false)
+  static let daging = Ingredient(imageName: "daging", name: "daging", isAbsurd: false)
+  static let mie = Ingredient(imageName: "mie", name: "mie", isAbsurd: false)
+  static let nasi = Ingredient(imageName: "nasi", name: "nasi", isAbsurd: false)
+  static let chili = Ingredient(imageName: "cabai", name: "cabai", isAbsurd: false)
 
   // Absurd ingredients (traps)
-  static let trapChili = Ingredient(imageName: "trapChilli", name: "trap chili", isAbsurd: true)
-  static let trapFishBone = Ingredient(imageName: "trapFishBone", name: "trap fish bone", isAbsurd: true)
+  static let trapFishBone = Ingredient(imageName: "fishBone", name: "fish bone", isAbsurd: true)
 
   static let recipes = [
     Recipe(name: "Gado-Gado", ingredients: [
       cabbage: 2,
       kacangPanjang: 2,
-      kacang: 1,
-      lontong: 2,
-      tahu: 1
-    ]),
-    Recipe(name: "Ketoprak", ingredients: [
+      kacang: 4,
       lontong: 3,
-      tahu: 2,
-      kacang: 2,
-      krupuk: 1
+      tahu: 3
     ]),
-    Recipe(name: "Nasi Gudeg", ingredients: [
+    Recipe(name: "Bakso", ingredients: [
+      daging: 4,
+      mie: 2,
+      cabbage: 1,
+      telor: 1,
+      krupuk: 2
+    ]),
+    Recipe(name: "Mie Instan", ingredients: [
+      mie: 4,
       telor: 2,
-      tahu: 1,
-      krupuk: 2,
-      cabbage: 1
+      cabbage: 1,
+      chili: 1
+    ]),
+    Recipe(name: "Ayam Geprek", ingredients: [
+      nasi: 3,
+      telor: 2,
+      cabbage: 1,
+      chili: 3,
+      krupuk: 2
     ])
   ]
 
   static let allIngredients = [
     // Valid ingredients
-    cabbage, kacangPanjang, kacang, krupuk, lontong, tahu, telor,
+    cabbage, kacangPanjang, kacang, krupuk, lontong, tahu, telor, daging, mie, nasi,
     // Absurd ingredients (traps)
-    trapChili, trapFishBone
+    chili, trapFishBone
   ]
 }
