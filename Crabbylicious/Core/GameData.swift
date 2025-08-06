b682e5b4db6500b23b398c9d1c4ea2b1a350ed2b
javaprada <javakanaya@gmail.com>
2025-08-06 22:18:02 +0700

    chore: gado-gado recipe to normal, delete unnecesary system

--- Crabbylicious/Core/GameData.swift
+++ Crabbylicious/Core/GameData.swift
@@ -27,11 +27,11 @@ class GameData {
 
   static let recipes = [
     Recipe(name: "Gado-Gado", ingredients: [
-      cabbage: 1
-//      kacangPanjang: 2,
-//      kacang: 4,
-//      lontong: 3,
-//      tahu: 3
+      cabbage: 3,
+      kacangPanjang: 2,
+      kacang: 4,
+      lontong: 3,
+      tahu: 3
     ]),
     Recipe(name: "Bakso", ingredients: [
       daging: 4,
